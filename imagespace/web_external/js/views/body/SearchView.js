danlamanna/image_space

commit ef302561d8ea13051cfa31f0746e10a641aa0b60

Stop fetching SearchView collection immediately

imagespace/web_external/js/views/body/SearchView.js
@@ -46,7 +46,6 @@ imagespace.views.SearchView = imagespace.View.extend({
 
             $('.alert-info').addClass('hidden');
         }, this));
-        this.collection.fetch(this.collection.params || {});
     },
 
     render: function () {
@@ -96,10 +95,12 @@ imagespace.router.route('search/:query(/params/:params)', 'search', function (qu
         imagespace.searchView.destroy();
     }
 
+    var coll = imagespace.getImageCollectionFromQuery(query);
     imagespace.searchView = new imagespace.views.SearchView({
-        collection: imagespace.getImageCollectionFromQuery(query),
+        collection: coll,
         parentView: window.app
     });
+    coll.fetch(coll.params || {});
 });
 
 imagespace.router.route('search/:url/:mode(/params/:params)', 'search', function (url, mode, params) {

commit 6cdf1657ae01bd3989c3c33dea8c717831998ff0

Fix broken similarity searches
This was broken in ef30256 because we stopped fetching collections by
default. When looking for instantiations of the SearchView I did not
think to look for its use as a symbol.

imagespace/web_external/js/views/body/SearchView.js
@@ -146,12 +146,14 @@ imagespace.router.route('search/:url/:mode(/params/:params)', 'search', function
         });
         $('.alert-info').html('Performing ' + niceName  + ' search <i class="icon-spin5 animate-spin"></i>').removeClass('hidden');
 
+        var coll = imagespace.searches[mode].search(image);
         girder.events.trigger('g:navigateTo', imagespace.views.SearchView, {
-            collection: imagespace.searches[mode].search(image),
+            collection: coll,
             searchImage: image,
             url: url,
             mode: mode
         });
+        coll.fetch(coll.params || {});
 
         imagespace.userDataView.render();
 